87e2924551ae5b8ac803122740d4a8003b3b9b8c
trinalampa <lampatrina142@gmail.com>
2026-04-08 03:31:04 +0800

    lab-act-1

---
+++ lampa-client/lab-act-1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: [Trina Mari S. Lampa]<br />
+          Email: [lampatrina142@gmail.com]<br />
+          Other Personal Info: [sikrit]
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
